f9fec1f7d06d65cfdfbdabbb8187c58d96f9c1aa
David Jones <dgjones@gmail.com>
2011-07-07 16:52:15 +1200

    simplified helper method with options and defaults now merged on model

--- app/helpers/copywriting_helper.rb
+++ app/helpers/copywriting_helper.rb
@@ -1,11 +1,11 @@
 module CopywritingHelper
+  
   def copywriting(name, options = {}, &block)
-    scope   = options[:scope] || 'default'
-    page_id = options[:page_id] || options[:page].try(:id) || nil
-    default = block_given? ? capture(&block) : options[:default]
+    options[:default] = block_given? ? capture(&block) : options[:default]
 
-    result = ::CopywritingPhrase.for(name, scope, default, page_id)
-    result = result.html_safe if options[:html_safe]
-    result
+    result = ::CopywritingPhrase.for(name, options)
+
+    options[:html_safe] ? result.html_safe : result
   end
+
 end
