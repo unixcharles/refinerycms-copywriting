ae829fbc1263a919c6dcc59c56f68ccc00961701
Charles Barbier <unixcharles@gmail.com>
2011-07-11 11:51:05 -0400

    should query with the page id, because the same key might exist for many pages

--- app/models/copywriting_phrase.rb
+++ app/models/copywriting_phrase.rb
@@ -9,15 +9,16 @@ class CopywritingPhrase < ActiveRecord::Base
   def self.for(name, options = {})
     options = {:phrase_type => 'text', :scope => 'default'}.merge(options)
     name = name.to_s
+    page_id = (options[:page].try(:id) || options[:page_id] || nil)
 
-    if (phrase = self.where(:name => name).first).nil?
+    if (phrase = self.where(:name => name, :page_id => page_id).first).nil?
       phrase = self.create(:name => name,
                            :scope => options[:scope],
                            :default => options[:default],
-                           :page_id => (options[:page].try(:id) || options[:page_id] || nil),
+                           :page_id => page_id,
                            :phrase_type => options[:phrase_type])
     end
-    
+
     phrase.default_or_value
   end
 
