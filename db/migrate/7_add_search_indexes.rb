c420850ac0433e979b653161fbf0364bd4c70676
Rob Holland <rob@initforthe.com>
2012-06-25 13:59:32 +0100

    Avoid long index names.

--- db/migrate/7_add_search_indexes.rb
+++ db/migrate/7_add_search_indexes.rb
@@ -1,6 +1,6 @@
 class AddSearchIndexes < ActiveRecord::Migration
   def change
-    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :target_type, :target_id], unique: true
-    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :page_id], unique: true
+    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :target_type, :target_id], unique: true, name: 'by_scoped_target'
+    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :page_id], unique: true, name: 'by_scoped_page'
   end
 end
