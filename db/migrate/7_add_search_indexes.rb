217b36215ac9a0fd81e3b21c96360f9f6bf63530
Rob Holland <rob@initforthe.com>
2012-06-25 14:39:35 +0100

    Fix index.

--- db/migrate/7_add_search_indexes.rb
+++ db/migrate/7_add_search_indexes.rb
@@ -1,6 +1,5 @@
 class AddSearchIndexes < ActiveRecord::Migration
   def change
-    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :target_type, :target_id], unique: true, name: 'by_scoped_target'
-    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :page_id], unique: true, name: 'by_scoped_page'
+    add_index Refinery::Copywriting::Phrase.table_name, [:name, :scope, :page_id, :target_type, :target_id], unique: true, name: 'by_scoped_page_or_target'
   end
 end
