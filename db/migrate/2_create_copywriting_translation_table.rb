e01dc8ae4ae2e4f09f73b05202988b8854bd7691
Martin Bilski <martinb@criticue.com>
2013-06-21 14:21:07 +0200

    Fixes issue #45 'rake db:migrate' - incompatible with SQLite

--- db/migrate/2_create_copywriting_translation_table.rb
+++ db/migrate/2_create_copywriting_translation_table.rb
@@ -13,7 +13,9 @@ class CreateCopywritingTranslationTable < ActiveRecord::Migration
       :migrate_data => true
     })
 
-    rename_column Refinery::Copywriting::Phrase.translation_class.table_name, :copywriting_phrase_id, :refinery_copywriting_phrase_id
+    with_short_sqlite_compatible_index do
+      rename_column Refinery::Copywriting::Phrase.translation_class.table_name, :copywriting_phrase_id, :refinery_copywriting_phrase_id
+    end
 
     ::Refinery::Copywriting::Phrase.table_name = "refinery_#{Refinery::Copywriting::Phrase.table_name}"
     ::Refinery::Copywriting::Phrase.module_eval do
@@ -36,6 +38,17 @@ class CreateCopywritingTranslationTable < ActiveRecord::Migration
       has_many :translations, :foreign_key => 'copywriting_phrase_id'
     end
     ::Refinery::Copywriting::Phrase.translation_class.table_name = "refinery_#{Refinery::Copywriting::Phrase.translation_class.table_name}"
+  end
+
+  
+  def self.with_short_sqlite_compatible_index
+    if ActiveRecord::Base.connection.adapter_name.downcase.to_sym == :sqlite
+      remove_index Refinery::Copywriting::Phrase.translation_class.table_name, [:copywriting_phrase_id]#[:index_altered_copywriting_phrase_translations_on_copywriting_phrase_id] 
+      yield
+      add_index Refinery::Copywriting::Phrase.translation_class.table_name, :refinery_copywriting_phrase_id, :name => "index_short_on_refinery_copywriting_phrase_id"
+    else
+      yield
+    end
   end
 
 end
