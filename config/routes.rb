f31abe35097fbd25236f56eb7cada20591af1559
Isaac Freeman <isaac@resolvedigital.co.nz>
2014-05-08 17:06:49 +1200

    Initial implementation for reorderable phrases
    - Uses standard RefineryCMS JS to reorder phrases
    - Phrases grouped by scope
    - Pagination currently disabled

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Refinery::Core::Engine.routes.draw do
   namespace :copywriting, :path => '' do
     namespace :admin, :path => 'refinery' do
       scope :path => 'copywriting' do
-        resources :phrases, :except => [:show, :new, :create]
+        resources :phrases, :except => [:show, :new, :create] do
+          collection do
+            post :update_positions
+          end
+        end
       end
     end
-  end
+  end  
 end
