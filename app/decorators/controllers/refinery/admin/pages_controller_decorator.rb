7ff78906c9238dbe070207e6608a010b6f498111
Brice Sanchez <brice@bisscomm.com>
2015-06-30 15:13:43 -0400

    Update engine for refinerycms 3.0.0
    * Add new dependencies
    * Use new way to precompile assets
    * Use new icons set in admin
    * Use new way to permit params

--- app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
@@ -1,12 +1,8 @@
-Refinery::Admin::PagesController.class_eval do
-
-  # Add :copywriting_phrases_attributes to page_params for strong parameters.
-  def page_params_with_copywriting_phrases_params
-    phrases_params = params.require(:page).permit(
-      copywriting_phrases_attributes: [:id, :value]
-    )
-    page_params_without_copywriting_phrases_params.merge(phrases_params)
+module RefineryCopywritingControllerDecorator
+  def permitted_page_params
+    params[:page][:copywriting_phrases_attributes]={} if params[:page][:copywriting_phrases_attributes].nil?
+    super <<  [copywriting_phrases_attributes: [:id, :value]]
   end
-  alias_method_chain :page_params, :copywriting_phrases_params
-
 end
+
+Refinery::Admin::PagesController.send :prepend, RefineryCopywritingControllerDecorator
