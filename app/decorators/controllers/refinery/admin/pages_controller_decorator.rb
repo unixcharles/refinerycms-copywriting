6d074958aaff4663ff127ec7348e212bc0986efc
Brice Sanchez <brice@bisscomm.com>
2015-07-06 10:17:43 -0400

    Improve permitted_page_params method
    It can now override false too. Thanks @unixcharles
    
    Remove useless nil? check in permitted_page_params

--- app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
@@ -1,6 +1,6 @@
 module RefineryCopywritingControllerDecorator
   def permitted_page_params
-    params[:page][:copywriting_phrases_attributes]={} if params[:page][:copywriting_phrases_attributes].nil?
+    params[:page][:copywriting_phrases_attributes] ||= {}
     super <<  [copywriting_phrases_attributes: [:id, :value]]
   end
 end
